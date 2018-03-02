f064a7b46454ef2dd0b81956b7a73d5eb7aa9537
leoteixeira-ig <leonardo.teixeira@infoglobo.com.br>
2018-03-02 11:37:22 -0300

    Refactoring: "getSchemaById" is now async

--- routes/get-schema-by-id.js
+++ routes/get-schema-by-id.js
@@ -1,10 +1,10 @@
 const errors = require('restify-errors');
 const cache = require('../lib/data-cache');
 
-module.exports = function getSchemaById(request, response, next) {
+module.exports = async function getSchemaById(request, response, next) {
     try {
         const id = request.params.id;
-        response.send(cache.loadById(id));
+        response.send(await cache.loadById(id));
         next();
     } catch(error) {
         next(new errors.InternalError(error.message));
