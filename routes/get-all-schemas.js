8119480b5e3194aaeaf605f8581821095aef3c54
leoteixeira-ig <leonardo.teixeira@infoglobo.com.br>
2018-03-09 18:42:56 -0300

    Refactoring: Wrapped collection in HAL response

--- routes/get-all-schemas.js
+++ routes/get-all-schemas.js
@@ -1,10 +1,25 @@
+const hal = require('hal');
+const moment = require('moment');
 const errors = require('restify-errors');
 const schemas = require('../lib/data-schema');
+
+function wrapCollectionInHal(obj) {
+    const resource = hal.Resource({
+        total: obj.length,
+        at: moment()
+    }, '/schema/');
+    resource.link('pick', {href: '/schema/{id}', templated: true});
+    resource.link('update', {href: '/schema/{id}', templated: true});
+    resource.link('new', {href: '/schema/{id}', templated: true});
+    resource.link('validate', {href: '/schema/{id}/instance', templated: true});
+    resource.embed('schemas', obj);
+    return resource;
+}
 
 module.exports = async function getAllSchemas(request, response, next) {
     try {
         const items = await schemas.findAll();
-        response.send(items);
+        response.send(wrapCollectionInHal(items));
         next();
     } catch(error) {
         next(new errors.InternalError(error.message));
