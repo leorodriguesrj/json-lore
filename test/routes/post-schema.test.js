09316fadbf5cf31c319b81c8c75f1b3af1ac0243
leoteixeira-ig <leonardo.teixeira@infoglobo.com.br>
2018-03-15 11:03:40 -0300

    QA: Covered postSchema

--- test/routes/post-schema.test.js
+++ test/routes/post-schema.test.js
@@ -1,4 +1,59 @@
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+chai.use(require('sinon-chai'));
+
+const sandbox = sinon.createSandbox();
+
+const sendInvocation = sandbox.stub();
+const registerInvocation = sandbox.stub();
+const wrapSchemaInvocation = sandbox.stub();
+const inferIdInvocation = sandbox.stub();
+const nextInvocation = sandbox.stub();
+
+const putSchema = proxyquire('../../routes/post-schema', {
+    './hal-wrapper': {wrapSchema: wrapSchemaInvocation},
+    '../lib/bus-schema': {
+        inferId: inferIdInvocation,
+        register: registerInvocation
+    }
+});
+
+const body = {};
+const wrappedBody = {};
+const request = {body: {body}};
+const response = {send: sendInvocation};
+
 describe('routes/post-schema', () => {
-    it('Should respond with saved representation of the schema.');
-    it('Should respond with "500 Internal error" if an exception occurs.');
+    
+    afterEach(() => sandbox.reset());
+
+    it('Should respond with the saved schema.', async () => {
+        inferIdInvocation.returns('y');
+        wrapSchemaInvocation.returns(wrappedBody);
+
+        await putSchema(request, response, nextInvocation);
+
+        expect(inferIdInvocation).to.be.calledWith(body);
+        expect(registerInvocation).to.be.calledWith('y', body);
+        expect(wrapSchemaInvocation).to.be.calledWith('y', body);
+        expect(sendInvocation).to.be.calledWith(wrappedBody);
+        expect(nextInvocation).to.be.calledOnce;
+    });
+
+    it('Should respond with HTTP 500 if an exception occurs.', async () => {
+        inferIdInvocation.throwsException(Error);
+
+        await putSchema(request, response, nextInvocation);
+
+        expect(inferIdInvocation).to.be.calledWith(body);
+        expect(registerInvocation).to.not.be.called;
+        expect(wrapSchemaInvocation).to.not.be.called;
+        expect(sendInvocation).to.not.be.called;
+        expect(nextInvocation).to.be.calledWithMatch(error => {
+            return error && error.statusCode === 500;
+        });
+    });
 });
