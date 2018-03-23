1e2ef8aed2ff5fbfe68f79fe325fe05d02291e43
leoteixeira-ig <leonardo.teixeira@infoglobo.com.br>
2018-03-23 20:47:46 -0300

    QA: Refactoring: Test simplification

--- test/routes/hal-wrapper.test.js
+++ test/routes/hal-wrapper.test.js
@@ -33,26 +33,23 @@ describe('routes/hal-wrapper', () => {
         it('Should return a resource with the "validate" link.');
     });
     describe('wrapValidationOutcome', () => {
-        beforeEach(() => sandbox.reset());
+        sandbox.reset();
+        momentInvocation.returns('now');
+        const subject = halWrapper.wrapValidationOutcome('x', {a: 112358});
         it('Should return a resource with a "body".', () => {
-            const subject = halWrapper.wrapValidationOutcome('y', {a: 112358});
             expect(subject).haveOwnProperty('body');
             expect(subject.body).to.be.deep.equal({a: 112358});
         });
         it('Should return a resource with the timestamp of operation.', () => {
-            momentInvocation.returns('now');
-            const subject = halWrapper.wrapValidationOutcome('x', {a: 1});
             expect(subject).haveOwnProperty('at');
             expect(subject.at).to.be.equal('now');
         });
         it('Should return a resource with the "self" link.', () => {
-            const subject = halWrapper.wrapValidationOutcome('x', {a: 1});
             expect(subject._links).haveOwnProperty('self');
             expect(subject._links.self.rel).to.be.equal('self');
             expect(subject._links.self.href).to.be.equal('/schema/x/instance');
         });
         it('Should return a resource with the "schema" link.', () => {
-            const subject = halWrapper.wrapValidationOutcome('x', {a: 1});
             expect(subject._links).haveOwnProperty('schema');
             expect(subject._links.schema.rel).to.be.equal('schema');
             expect(subject._links.schema.href).to.be.equal('/schema/x');
