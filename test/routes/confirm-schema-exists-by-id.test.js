64abbc9b84118244283acce1174a0b5a45c5e5ba
leoteixeira-ig <leonardo.teixeira@infoglobo.com.br>
2018-03-29 09:34:35 -0300

    QA: Covered confirmSchemaExistsById

--- test/routes/confirm-schema-exists-by-id.test.js
+++ test/routes/confirm-schema-exists-by-id.test.js
@@ -1,5 +1,66 @@
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+chai.use(require('sinon-chai'));
+
+const sandbox = sinon.createSandbox();
+
+const existsInvocation = sandbox.stub();
+const nextInvocation = sandbox.stub();
+
+const {NotFoundError, InternalError} = require('restify-errors');
+const confirmSchemaExistsById = proxyquire('../../routes/confirm-schema-exists-by-id', {
+    '../lib/data-schema': {
+        exists: existsInvocation
+    }
+});
+
+const request = {params: {id: 'xpto'}};
+const response = {};
+
 describe('routes/confirm-schema-exists-by-id', () => {
-    it('Should continue to next step if schema is found.');
-    it('Should fail with "404 Not found" if schema is NOT found.');
-    it('Should fail with "500 Internal error" if an exception occurs.');
+    afterEach(() => sandbox.reset());
+
+    it('Should continue to next step if schema is found.', async () => {
+        existsInvocation.returns(true);
+
+        await confirmSchemaExistsById(request, response, nextInvocation);
+
+        expect(existsInvocation)
+            .to.be.calledOnce
+            .and.to.be.calledWith('xpto');
+            
+        expect(nextInvocation)
+            .to.be.calledOnce;
+    });
+    it('Should fail with HTTP 404 if schema is NOT found.', async () => {
+        existsInvocation.returns(false);
+
+        await confirmSchemaExistsById(request, response, nextInvocation);
+
+        expect(existsInvocation)
+            .to.be.calledOnce
+            .and.to.be.calledWith('xpto');
+            
+        expect(nextInvocation)
+            .to.be.calledOnce
+            .and.to.be.calledWithMatch(error =>
+                error instanceof NotFoundError);
+    });
+    it('Should fail with HTTP 500 if an exception occurs.', async () => {
+        existsInvocation.throws(Error);
+
+        await confirmSchemaExistsById(request, response, nextInvocation);
+
+        expect(existsInvocation)
+            .to.be.calledOnce
+            .and.to.be.calledWith('xpto');
+
+        expect(nextInvocation)
+            .to.be.calledOnce
+            .and.to.be.calledWithMatch(error =>
+                error instanceof InternalError);
+    });
 });
