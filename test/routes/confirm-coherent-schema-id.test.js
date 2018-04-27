962ff13cd3caf91541ef6e8ef63823eec32b76a2
leoteixeira-ig <leonardo.teixeira@infoglobo.com.br>
2018-04-27 11:48:16 -0300

    QA: Covered confirmCoherentSchemaId

--- test/routes/confirm-coherent-schema-id.test.js
+++ test/routes/confirm-coherent-schema-id.test.js
@@ -1,6 +1,66 @@
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+chai.use(require('sinon-chai'));
+
+const sandbox = sinon.createSandbox();
+const inferIdInvocation = sandbox.stub();
+const nextInvocation = sandbox.stub();
+
+const {InternalError, ConflictError} = require('restify-errors');
+const confirmCoherentSchemaId = proxyquire(
+    '../../routes/confirm-coherent-schema-id',
+    {'../lib/bus-schema': {inferId: inferIdInvocation}}
+);
+
+const innerBody = {};
+const request = {body: {body: innerBody}};
+const response = {};
+
 describe('routes/confirm-coherent-schema-id', () => {
-    it('Should continue to next step if body has no "id"');
-    it('Should continue to next step if body and URI have the same "id"');
-    it('Should fail with "409 Conflict" if "body.id" differs from URL\'s "id"');
-    it('Should fail with "500 Internal error" in case of exception');
+    afterEach(() => sandbox.reset());
+
+    it('Should continue if body has no "id"', async () => {
+        inferIdInvocation.returns(undefined);
+        await confirmCoherentSchemaId(request, response, nextInvocation);
+        expect(inferIdInvocation)
+            .to.be.calledOnce
+            .and.to.be.calledWith(innerBody);
+        expect(nextInvocation)
+            .to.be.calledOnce;
+    });
+    it('Should continue if body and URI have the same "id"', async () => {
+        inferIdInvocation.returns('z');
+        const innerBody = {};
+        const localRequest = {body: {body: innerBody}, params: {id: 'z'}};
+        await confirmCoherentSchemaId(localRequest, response, nextInvocation);
+        expect(inferIdInvocation)
+            .to.be.calledOnce
+            .and.to.be.calledWith(innerBody);
+        expect(nextInvocation)
+            .to.be.calledOnce;
+    });
+    it('Should fail with HTTP 409 if body and URL ids differ', async () => {
+        inferIdInvocation.returns('x');
+        const innerBody = {};
+        const localRequest = {body: {body: innerBody}, params: {id: 'y'}};
+        await confirmCoherentSchemaId(localRequest, response, nextInvocation);
+        expect(inferIdInvocation)
+            .to.be.calledOnce
+            .and.to.be.calledWith(innerBody);
+        expect(nextInvocation)
+            .to.be.calledOnce
+            .and.to.be.calledWithMatch(error =>
+                error instanceof ConflictError);
+    });
+    it('Should fail with HTTP 500 if an exception occurs', async () => {
+        inferIdInvocation.throwsException(Error);
+        await confirmCoherentSchemaId(request, response, nextInvocation);
+        expect(nextInvocation)
+            .to.be.calledOnce
+            .and.to.be.calledWithMatch(error =>
+                error instanceof InternalError);
+    });
 });
