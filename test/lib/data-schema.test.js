22f5fda35c7c1fcdaec5942cc24ad8c6a4dd097d
leoteixeira-ig <leonardo.teixeira@infoglobo.com.br>
2018-06-06 11:48:02 -0300

    QA: Covered dataSchema::findAll (success)

--- test/lib/data-schema.test.js
+++ test/lib/data-schema.test.js
@@ -1,6 +1,55 @@
+
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+chai.use(require('sinon-chai'));
+
+const sandbox = sinon.createSandbox();
+
+const readdirSub = sandbox.stub();
+const readFileStub = sandbox.stub();
+
+const dataSchema = proxyquire('../../lib/data-schema', {
+    'fs': {
+        'readdir': (path, callback) => {
+            const {error, data} = readdirSub(path);
+            callback(error, data);
+        },
+        'readFile': (path, callback) => {
+            const {error, data} = readFileStub(path);
+            callback(error, data);
+        }
+    }
+});
+
 describe('lib/data-schema', () => {
     describe('findAll', () => {
-        it('Should load the three files listed by fs');
+        afterEach(() => sandbox.reset());
+
+        it('Should load the two files listed by fs', async () => {
+            readdirSub.returns({
+                error: undefined, data: ['schema1.json', 'schema2.json']
+            });
+
+            readFileStub.onCall(0).returns({
+                error: undefined, data: '{"someName": "someValue"}'
+            });
+
+            readFileStub.onCall(1).returns({
+                error: undefined, data: '{"anotherName": "anotherValue"}'
+            });
+
+            const schemas = await dataSchema.findAll();
+
+            expect(readdirSub).to.be.calledOnce;
+            expect(readFileStub).to.be.calledTwice;
+            expect(schemas).to.be.deep.equals([
+                { someName: 'someValue' },
+                { anotherName: 'anotherValue' }
+            ]);
+        });
     });
     describe('pickById', () => {
         it('Should load the contents of file with id "z"');
